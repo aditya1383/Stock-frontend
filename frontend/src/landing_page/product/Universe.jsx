1174ee4d363134ad51a7c0d9feae18b03212c2a8
aditya1383 <aditchaudhary61@gmail.com>
2025-08-02 15:21:13 +0530

    Update Universe.jsx
    dhansetu change

--- frontend/src/landing_page/product/Universe.jsx
+++ frontend/src/landing_page/product/Universe.jsx
@@ -4,7 +4,7 @@ const Universe = () => {
     return (
         <div className="container">
             <div className="row text-center">
-                <h1>The Zerodha Universe</h1>
+                <h1>The Dhansetu Universe</h1>
                 <p className="text-muted">Extend your trading and investment experience even further with our partner platforms</p>
 
                 <div className="col-4 p-3 mt-5">
